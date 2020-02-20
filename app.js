02f9dbf39480393be68acfe1db057f66d0543af2
arxcdr <34399111+arxcdr@users.noreply.github.com>
2020-02-20 18:10:44 +0100

    - implement deployment fixes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'client/build', 'index.ejs'));
 });
 
 app.get("/", function(req, res){
